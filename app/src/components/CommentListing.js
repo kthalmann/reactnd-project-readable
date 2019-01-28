5c1d3846de10f51d972725f1595be8a098438f02
kevinthalmann <kevin.thalmann@gmail.com>
2019-01-28 19:13:21 -0200

    fix: add key property to Comment component

--- app/src/components/CommentListing.js
+++ app/src/components/CommentListing.js
@@ -1,12 +1,15 @@
 import React, { Fragment } from 'react'
 import Comment from './Comment'
 
-export default function CommentListing({ comments }) {
+export default function CommentListing({ comments, loading }) {
   return (
     <Fragment>
       <h3>Comments</h3>
       <div className="comment-listing child-borders">
-        {comments.map(comment => <Comment comment={comment} />)}
+        {loading && <div>loading...</div>}
+        {comments.map(comment => (
+          <Comment key={comment.id} comment={comment} />
+        ))}
       </div>
     </Fragment>
   )
