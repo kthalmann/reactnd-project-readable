0f5b303c5eb325647e2cbfc4ff2b90aa21274115
kevinthalmann <kevin.thalmann@gmail.com>
2019-01-28 21:53:37 -0200

    feat: api method for voting on comments, add headers

--- app/src/utils/api.js
+++ app/src/utils/api.js
@@ -3,7 +3,9 @@ const api = 'http://localhost:3001'
 const token = 'loremipsum'
 
 const headers = {
-  Authorization: token
+  Authorization: token,
+  Accept: 'application/json',
+  'Content-Type': 'application/json'
 }
 
 export const _getCategories = _ => {
@@ -22,4 +24,12 @@ export const _getCommentsForPost = postId => {
   return fetch(`${api}/posts/${postId}/comments`, { headers })
     .then(res => res.json())
     .then(data => data)
+}
+
+export const _voteOnComment = (commentId, isUpvote) => {
+  return fetch(`${api}/comments/${commentId}`, {
+    method: 'POST',
+    headers,
+    body: JSON.stringify({ option: isUpvote ? 'upVote' : 'downVote' })
+  }).then(res => res.json())
 }
