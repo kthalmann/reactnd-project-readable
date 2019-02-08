58fa2427eb237fb4aa1dcf1c0a1fbf753e9cfdfa
kevinthalmann <kevin.thalmann@gmail.com>
2019-02-08 13:40:25 -0200

    feat: back link on 404 page

--- app/src/components/404.js
+++ app/src/components/404.js
@@ -1,5 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Page404(props) {
-  return <h1 className="page404">404 Page not found</h1>
+  return (
+    <div className="page404">
+      <h1>404 Page not found</h1>
+      <Link to="/" className="paper-btn">
+        Back to Start
+      </Link>
+    </div>
+  )
 }
