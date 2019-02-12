9afa1a32f8bee1bf2b8e9921a6f88fcfbee01da8
kevinthalmann <kevin.thalmann@gmail.com>
2019-02-12 10:13:43 -0200

    feat: use mapDispatchToProps method in PostListing component und make it a stateless functional component

--- app/src/components/PostListing.js
+++ app/src/components/PostListing.js
@@ -1,49 +1,41 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { DEFAULT_POST_SORT_METHOD } from '../actions/shared';
 import { setPostSortMethod } from '../actions/postSorting';
 import PostTeaser from './PostTeaser';
 
-class PostListing extends Component {
-  onChange = newSorting => {
-    this.props.dispatch(setPostSortMethod(newSorting));
-  };
+function PostListing({ posts, postSorting, onSortMethodChange }) {
+  // sort posts by sort method and return only ids
+  const postIds = posts
+    .slice()
+    .sort((a, b) => b[postSorting] - a[postSorting])
+    .map(post => post.id);
 
-  render() {
-    const { posts, postSorting } = this.props;
-
-    // sort posts by sort method and return only ids
-    const postIds = posts
-      .slice()
-      .sort((a, b) => b[postSorting] - a[postSorting])
-      .map(post => post.id);
-
-    return (
-      <div className="post-listing">
-        <div className="post-listing__header row flex-edges flex-middle">
-          <h2 className="post-listing__title">Posts</h2>
-          <div className="post-listing__sorting form-group">
-            <label className="post-listing__sort-label" htmlFor="sortMethod">
-              Sort by:
-            </label>
-            <select
-              id="sortMethod"
-              className="post-listing__sort-input"
-              value={postSorting || DEFAULT_POST_SORT_METHOD}
-              onChange={e => this.onChange(e.target.value)}
-            >
-              <option value="timestamp">Age</option>
-              <option value="voteScore">Score</option>
-            </select>
-          </div>
+  return (
+    <div className="post-listing">
+      <div className="post-listing__header row flex-edges flex-middle">
+        <h2 className="post-listing__title">Posts</h2>
+        <div className="post-listing__sorting form-group">
+          <label className="post-listing__sort-label" htmlFor="sortMethod">
+            Sort by:
+          </label>
+          <select
+            id="sortMethod"
+            className="post-listing__sort-input"
+            value={postSorting || DEFAULT_POST_SORT_METHOD}
+            onChange={e => onSortMethodChange(e.target.value)}
+          >
+            <option value="timestamp">Age</option>
+            <option value="voteScore">Score</option>
+          </select>
         </div>
-        {postIds.length === 0 && (
-          <div className="alert alert-warning">No posts to display</div>
-        )}
-        {postIds.map(post => <PostTeaser key={post} postId={post} />)}
       </div>
-    );
-  }
+      {postIds.length === 0 && (
+        <div className="alert alert-warning">No posts to display</div>
+      )}
+      {postIds.map(post => <PostTeaser key={post} postId={post} />)}
+    </div>
+  );
 }
 
 function mapStateToProps({ postSorting }, { posts }) {
@@ -53,4 +45,12 @@ function mapStateToProps({ postSorting }, { posts }) {
   };
 }
 
-export default connect(mapStateToProps)(PostListing);
+function mapDispatchToProps(dispatch) {
+  return {
+    onSortMethodChange: newSorting => {
+      dispatch(setPostSortMethod(newSorting));
+    }
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostListing);
