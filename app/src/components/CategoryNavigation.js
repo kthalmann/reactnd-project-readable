7bfdfc781609f10857700bac86fd5a435dc1d493
kevinthalmann <kevin.thalmann@gmail.com>
2019-02-12 09:25:27 -0200

    feat: use stateless functional component for CategoryNavigation

--- app/src/components/CategoryNavigation.js
+++ app/src/components/CategoryNavigation.js
@@ -1,45 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { NavLink, Link, withRouter } from 'react-router-dom';
 import { capitalize } from '../utils';
 
-class CategoryNavigation extends Component {
-  render() {
-    return (
-      <nav className="category-nav border fixed split-nav">
-        <div className="nav-brand">
-          <h3>
-            <Link to="/">Readable</Link>
-          </h3>
+function CategoryNavigation({ categories }) {
+  return (
+    <nav className="category-nav border fixed split-nav">
+      <div className="nav-brand">
+        <h3>
+          <Link to="/">Readable</Link>
+        </h3>
+      </div>
+      <div className="collapsible">
+        <input id="collapsible1" type="checkbox" name="collapsible1" />
+        <button>
+          <label htmlFor="collapsible1">
+            <div className="bar1" />
+            <div className="bar2" />
+            <div className="bar3" />
+          </label>
+        </button>
+        <div className="collapsible-body">
+          <ul className="inline">
+            {categories.map(category => (
+              <li key={category.name}>
+                <NavLink
+                  to={`/${category.name}`}
+                  className="category-nav__link"
+                  activeClassName="active"
+                >
+                  {capitalize(category.name)}
+                </NavLink>
+              </li>
+            ))}
+          </ul>
         </div>
-        <div className="collapsible">
-          <input id="collapsible1" type="checkbox" name="collapsible1" />
-          <button>
-            <label htmlFor="collapsible1">
-              <div className="bar1" />
-              <div className="bar2" />
-              <div className="bar3" />
-            </label>
-          </button>
-          <div className="collapsible-body">
-            <ul className="inline">
-              {this.props.categories.map(category => (
-                <li key={category.name}>
-                  <NavLink
-                    to={`/${category.name}`}
-                    className="category-nav__link"
-                    activeClassName="active"
-                  >
-                    {capitalize(category.name)}
-                  </NavLink>
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-      </nav>
-    );
-  }
+      </div>
+    </nav>
+  );
 }
 
 function mapStateToProps({ categories }) {
